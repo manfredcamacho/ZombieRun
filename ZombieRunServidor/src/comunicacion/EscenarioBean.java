520cfb1b5547b53b01206a7bb7ecf51c0e3c7fba
laboratorios <laboratorios@lab3a1.unlmlab.edu.ar>
2015-07-08 14:51:29 -0300

    Clase EscenarioBean

---
+++ ZombieRunServidor/src/comunicacion/EscenarioBean.java
@@ -0,0 +1,32 @@
+package comunicacion;
+import java.io.Serializable;
+
+import clasesPrincipales.*;
+public class EscenarioBean implements Serializable {
+
+	
+	private static final long serialVersionUID = 8885287870629662484L;
+
+	private Figura[][] mapa;
+	private int tamX;
+	private int tamY;
+	
+	public EscenarioBean( Figura[][] m, int x, int y){
+		mapa = m;
+		tamX = x;
+		tamY = y;
+	}
+
+	public Figura[][] getMapa() {
+		return mapa;
+	}
+
+	public int getTamX() {
+		return tamX;
+	}
+
+	public int getTamY() {
+		return tamY;
+	}
+	
+}
